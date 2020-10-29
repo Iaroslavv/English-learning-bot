6ccf4dd9cc4817ba7e382a530bb2a8afe7b5de84
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-10-30 00:10:28 +0300

    Add forms for registration and login

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import SignUpForm, LoginForm
 
 
 @app.route("/main", methods=["GET", "POST"])
 def main():
     title = "Homepage"
     return render_template("layout.html", title=title)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash("Your account has been successfully created!", "success")
+        return redirect(url_for("main"))
+        
+    return render_template("signup.html", title="Sign up", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign up", form=form)
+
