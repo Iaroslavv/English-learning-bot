3dbab30e8d6ce9351fb68a84ce03c7171df82431
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-11-01 21:41:06 +0300

    Add update form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from app.models import User
+from flask_login import current_user
 
 
 class SignUpForm(FlaskForm):
@@ -31,3 +33,24 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember me")
     submit = SubmitField("Log in")
+
+             
+class UpdateAccountForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(),
+                                           Length(min=2, max=20)])
+    email = EmailField("Email", validators=[DataRequired()])
+    picture = FileField("Update Profile Picture", validators=[FileAllowed(["jpg", "png"])])
+    telegram_info = StringField("Telegram", validators=[DataRequired()])
+    submit = SubmitField("Update")
+    
+    def validate_name(self, name):
+        if name.data != current_user.name:
+            user = User.query.filter_by(name=name.data).first()
+            if user:
+                raise ValidationError("The user with this name already exists!")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError("The user with this email already exists!")                              
