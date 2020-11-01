ed924c115b672217b34793bcd0535dd708447cfe
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-11-01 21:41:13 +0300

    Add pic

--- app/models.py
+++ app/models.py
@@ -13,21 +13,24 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
+    img_file = db.Column(db.String(20), nullable=False, default="static/default.png")
     password = db.Column(db.String(60), nullable=False)
     words = db.relationship("Words", backref="author", lazy=True)
+    telegram_info = db.Column(db.String(30), nullable=False, default="@Yourtelegram")
 
     def __repr__(self):
         return f"User('{self.name}', '{self.email}')"
 
- 
+
 class Words(db.Model):
     """User's words db model."""
 
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(40), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    
+  
     def __repr__(self):
-        return f"AddWords('{self.word}')"
+        return f"Words('{self.word}')"
+
 
 # probably should add one more table to store the example exersises
