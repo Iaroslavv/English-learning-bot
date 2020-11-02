efa9cd910bfc59bc7fb5525f2acdb6f26ee66cb3
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-11-02 19:49:36 +0300

    Add password reset link

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from app import db, login_manager
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 
 @login_manager.user_loader
@@ -16,6 +18,19 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     words = db.relationship("Words", backref="author", lazy=True)
 
+    def get_reset_token(self, expires_sec=1800):
+        serial = Serializer(current_app.config["SECRET_KEY"], expires_sec)
+        return serial.dumps({"user_id": self.id}).decode("utf-8")
+
+    @staticmethod
+    def verify_reset_token(token):
+        serial = Serializer(current_app.config["SECRET_KEY"])
+        try:
+            user_id = serial.loads(token)["user_id"]
+        except Exception:
+            return None
+        return User.query.get(user_id)
+    
     def __repr__(self):
         return f"User('{self.name}', '{self.email}')"
 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from app import app, db, bcrypt
-from flask import render_template, flash, redirect, url_for, request
-from app.forms import SignUpForm, LoginForm
+from app import app, db, bcrypt, mail
+from flask import render_template, flash, redirect, url_for, request, current_app
+from app.forms import SignUpForm, LoginForm, RequestResetForm, ResetPasswordForm
 from app.models import User
 from flask_login import login_user, login_required, current_user, logout_user
+from flask_mail import Message
 
 
 @app.route("/main", methods=["GET", "POST"])
@@ -24,7 +25,7 @@ def signup():
 
         flash("Your account has been successfully created!", "success")    # not working
         return redirect(url_for("login"))
-       
+  
     return render_template("signup.html", title="Sign up", form=form)
 
 
@@ -54,11 +55,56 @@ def logout():
 @login_required
 def account():
     return render_template("account.html", title="Account")
-    
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    body = f'''To reset your password, visit the following link:
+{url_for("reset_token", token=token, _external=True)}
+  
+If you did not make this request then simply ignore this email.
+'''
+    msg = Message(
+        "Password reset request",
+        sender=current_app.config["MAIL_USERNAME"],
+        recipients=[user.email],
+        body=body,
+        )
+    mail.send(msg)
+ 
+
+@app.route("/reset_password", methods=["GET", "POST"])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for("main"))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash("An email has been sent with instructions to reset your password.", "info")
+        return redirect("login")
+    return render_template("reset_request.html", title="Reset Password", form=form)
+
+
+@app.route("/reset_password/<token>", methods=["GET", "POST"])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for("main"))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash("That is an invalid or expired token", "warning")
+        return redirect(url_for("reset_request"))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user.password = hashed_password
+        db.session.commit()
+        flash("Your password has been updated!", "success")    # not working
+        return redirect(url_for("login"))
+    return render_template("reset_token.html", title="Reset Password", form=form)
+  
 
 @app.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
     pass
-
-
