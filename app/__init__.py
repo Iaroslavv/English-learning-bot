b7aae532aa785f4cb0a51d7506bf11ae772410bc
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-11-03 23:45:29 +0300

    Fix bugs

--- app/__init__.py
+++ app/__init__.py
@@ -4,20 +4,30 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from app.config import Config
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object("config.config")
-db = SQLAlchemy(app)
-mail = Mail(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+mail = Mail()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = "users.login"
 login_manager.login_message_category = "info"
-db.init_app(app)
-migrate = Migrate(app, db)
-app.static_folder = "static"
-app.template_folder = "templates"
-from app import models
-from app.users.routes import users
-app.register_blueprint(users)
+# migrate = Migrate()
 
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(Config)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
+    app.static_folder = "static"
+    app.template_folder = "templates"
+    
+    from app import models
+    from app.users.routes import users
+    app.register_blueprint(users)
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,15 @@
+
+class Config:
+    DEBUG = True
+    CSRF_ENABLED = True
+    SECRET_KEY = 'gh3y4hgy48hghidbsla43gb'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///vocab.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = "iaroslavmusic@gmail.com"
+    MAIL_PASSWORD = "yaroslavolga1972"
+    TOKEN = "1286174748:AAEUM7BBZhl3ZyOfntfX38HbqmrzpO8Ks1g"
