09873b77fdbca9926ebd870dfe4a130e4e6fef52
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-11-11 23:56:28 +0300

    Add counting test results

--- app/telegram_bot/count_user_points.py
+++ app/telegram_bot/count_user_points.py
@@ -9,5 +9,16 @@ class TestCounter:
 
     @property
     def show_total(self):
-        return self.count
-  
+        if self.count <= 10:
+            total = f"Your level is beginner. {self.count}/50 are correct!"
+        if 10 < self.count <= 16:
+            total = f"Your level is elementary. {self.count}/50 are correct!"
+        if 16 < self.count <= 25:
+            total = f"Your level is pre-intermediate.{ self.count}/50 are correct!"
+        if 25 < self.count <= 34:
+            total = f"Your level is itermediate. {self.count}/50 are correct!"
+        if 34 < self.count <= 43:
+            total = f"Your level is upper-intermediate. {self.count}/50 are correct!"
+        if 43 < self.count <= 50:
+            total = f"Your level is advanced. {self.count}/50 are correct!"
+        return total
