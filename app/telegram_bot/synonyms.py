a43fafa9655ede0c0741f06a711ea0c56d0caa49
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-11-18 00:23:51 +0300

    add word parser

--- app/telegram_bot/synonyms.py
+++ app/telegram_bot/synonyms.py
@@ -1,15 +1,22 @@
 import requests as req
+import json
 
 
-def find_synonym(*word):
+def find_synonym(*args):
+    """Looks for the similar word."""
+    try:
+        for param in args:
+            url = f"https://wordsapiv1.p.rapidapi.com/words/{param}/synonyms"
 
-    url = "https://wordsapiv1.p.rapidapi.com/words/get/synonyms"
+            headers = {
+                'x-rapidapi-key': "7a861b97b1msh795315f8cc4e24dp17d59ajsn01d1cb5156cb",
+                'x-rapidapi-host': "wordsapiv1.p.rapidapi.com"
+                }
 
-    headers = {
-        'x-rapidapi-key': "7a861b97b1msh795315f8cc4e24dp17d59ajsn01d1cb5156cb",
-        'x-rapidapi-host': "wordsapiv1.p.rapidapi.com"
-        }
-
-    response = req.request("GET", url, headers=headers)
-
-    return response.text
+            response = req.request("GET", url, headers=headers)
+            get_response = response.text
+            answer = json.loads(get_response)
+            final_answer = answer["synonyms"]
+        return final_answer
+    except Exception as e:
+        return str(e)
